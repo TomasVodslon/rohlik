481da5300867e51a5caecd03f04ec5231c771894
Tomas Vodslon <tomas.vodslon@airbank.cz>
2021-08-12 08:02:36 +0200

    Add new paths to yaml

--- rohlik-app/src/main/java/cz/root/rohlik/controllers/OrderController.java
+++ rohlik-app/src/main/java/cz/root/rohlik/controllers/OrderController.java
@@ -6,6 +6,16 @@ import org.springframework.http.ResponseEntity;
 public class OrderController implements OrderApi {
 
     @Override
+    public ResponseEntity<Void> orderDelete() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> orderDelete(String id) {
+        return null;
+    }
+
+    @Override
     public ResponseEntity<Void> orderPost() {
         return null;
     }
